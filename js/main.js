6fdfc30b5fd9f370d57b304a157c506977f83ace
swepa <asim@swepa-web.com>
2013-10-25 10:09:47 +0200

    Handelbars Template Added.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@ var app = {
 
     findByName: function() {
         console.log('findByName');
+        var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+            /*
             var l = employees.length;
             var e;
             $('.employee-list').empty();
             for (var i=0; i<l; i++) {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            }*/
         });
     },
 
@@ -26,6 +29,7 @@ var app = {
     renderHomeView: function (){
       //var html = "";
         var self = this;
+        $('body').html(self.homeTpl);
       $('.search-key').on('keyup', $.proxy(self.findByName, self));
     },
 
@@ -47,8 +51,13 @@ var app = {
             //alert("device is ready, going to initilize Store.");
             self.showAlert('Store Initialized', 'Information.')
             self.initializeStore();
-        }
 
+            this.homeTpl = Handlebars.compile($("#home-tpl").html());
+            this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        }
+        //self.initializeStore();
+        //this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        //this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
     }
 
 };
